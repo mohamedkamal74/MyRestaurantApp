e3acca79a6dd30cd0f51df641e190515c010dc27
mohamed kamal <mkahf5103@gmail.com>
2022-07-31 19:25:22 +0400

    add category model and controller

---
+++ MyRestaurant/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRestaurant.Data;
+
+namespace MyRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyRestaurant/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRestaurant.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Category Name")]
+        public string Name { get; set; }
+    }
+}
