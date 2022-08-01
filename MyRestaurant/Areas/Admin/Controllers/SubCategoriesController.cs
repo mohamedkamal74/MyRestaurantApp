636a2cffc92e805ea73755d43f1f6d51eb2acb59
mohamed kamal <mkahf5103@gmail.com>
2022-08-01 22:20:27 +0400

    add create action to sub categories

--- MyRestaurant/Areas/Admin/Controllers/SubCategoriesController.cs
+++ MyRestaurant/Areas/Admin/Controllers/SubCategoriesController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyRestaurant.Data;
+using MyRestaurant.Models;
+using MyRestaurant.ViewModels;
 using NToastNotify;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyRestaurant.Areas.Admin.Controllers
 {
@@ -15,9 +20,21 @@ namespace MyRestaurant.Areas.Admin.Controllers
             _context = context;
             _toastNotification = toastNotification;
         }
-        public IActionResult Index()
+        public async Task<IActionResult>Index()
         {
-            return View();
+            var subcategory = await _context.SubCategories.Include(c => c.Category).OrderBy(x => x.Name).ToListAsync();
+            return View(subcategory);
+        }
+        public async Task<IActionResult> Create()
+        {
+            var categoryandcategoryVm = new CategoryAndSubcategoryViewModel
+            {
+                CategoriesList = await _context.Categories.ToListAsync(),
+                SubCategory = new SubCategory(),
+                SubCategoriesList = await _context.SubCategories.OrderBy(m => m.Name).Select(x => x.Name).Distinct().ToListAsync()
+
+            };
+            return View(categoryandcategoryVm);
         }
     }
 }
---
+++ MyRestaurant/ViewModels/CategoryAndSubcategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyRestaurant.Models;
+using System.Collections.Generic;
+
+namespace MyRestaurant.ViewModels
+{
+    public class CategoryAndSubcategoryViewModel
+    {
+        public IEnumerable<Category> CategoriesList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoriesList { get; set; }
+        public string StatusMessage { get; set; }
+
+
+    }
+}
