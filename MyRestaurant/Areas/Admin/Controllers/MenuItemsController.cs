f546e5913ee3e8f47f695598e7f0296c8c646359
mohamed kamal <mkahf5103@gmail.com>
2022-08-03 03:07:30 +0400

    add create view for menu item

--- MyRestaurant/Areas/Admin/Controllers/MenuItemsController.cs
+++ MyRestaurant/Areas/Admin/Controllers/MenuItemsController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyRestaurant.Data;
+using MyRestaurant.Models;
+using MyRestaurant.ViewModels;
 using NToastNotify;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyRestaurant.Areas.Admin.Controllers
@@ -12,15 +15,28 @@ namespace MyRestaurant.Areas.Admin.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IToastNotification _toastNotification;
 
+        [BindProperty]
+        public MenuItemViewmodel MenuItemVM { get; set; }
+
         public MenuItemsController(ApplicationDbContext context, IToastNotification toastNotification)
         {
             _context = context;
             _toastNotification = toastNotification;
+            MenuItemVM = new MenuItemViewmodel()
+            {
+                MenuItem=new MenuItem(),
+                CategoriesList=_context.Categories.ToList()
+            };
         }
         public async Task<IActionResult> Index()
         {
             var menuitems = await _context.MenuItems.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuitems);
+        }
+
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
---
+++ MyRestaurant/ViewModels/MenuItemViewmodel.cs
@@ -0,0 +1,12 @@
+﻿using MyRestaurant.Models;
+using System.Collections.Generic;
+
+namespace MyRestaurant.ViewModels
+{
+    public class MenuItemViewmodel
+    {
+        public MenuItem MenuItem { get; set; }
+        public IEnumerable<Category> CategoriesList { get; set; }
+        public IEnumerable<SubCategory> SubCategoriesList { get; set; }
+    }
+}
