2d49269814bd16ab3c56979b18acd67ad307355f
mohamed kamal <mkahf5103@gmail.com>
2022-08-04 03:06:00 +0400

    add copouns controller

---
+++ MyRestaurant/Areas/Admin/Controllers/CopounsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyRestaurant.Data;
+using NToastNotify;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CopounsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IToastNotification _toastNotification;
+
+        public CopounsController(ApplicationDbContext context, IToastNotification toastNotification)
+        {
+            _context = context;
+            _toastNotification = toastNotification;
+        }
+        public async Task< IActionResult> Index()
+        {
+            return View(await _context.Copouns.ToListAsync());
+        }
+    }
+}
