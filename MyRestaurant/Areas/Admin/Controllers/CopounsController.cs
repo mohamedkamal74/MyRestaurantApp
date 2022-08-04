8b4111fd7f005cd2f9c32fa7f2f91601b531650f
mohamed kamal <mkahf5103@gmail.com>
2022-08-04 19:07:11 +0400

    add create and edit copoun

--- MyRestaurant/Areas/Admin/Controllers/CopounsController.cs
+++ MyRestaurant/Areas/Admin/Controllers/CopounsController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using MyRestaurant.Data;
 using MyRestaurant.Models;
 using NToastNotify;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MyRestaurant.Areas.Admin.Controllers
@@ -26,6 +27,58 @@ namespace MyRestaurant.Areas.Admin.Controllers
         public  IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Create(Copoun model)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files != null)
+                {
+                    var filestream = files[0].OpenReadStream();
+                    var memorystream = new MemoryStream();
+                    filestream.CopyTo(memorystream);
+                    model.Picture = memorystream.ToArray();
+                }
+                _context.Copouns.Add(model);
+                await _context.SaveChangesAsync();
+                _toastNotification.AddSuccessToastMessage("Copoun Created Succesfully");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task< IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var copoun =await _context.Copouns.FindAsync(id);
+            if (copoun == null)
+                return NotFound();
+            return View(copoun);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Copoun model)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files .Count>0)
+                {
+                    var filestream = files[0].OpenReadStream();
+                    var memorystream = new MemoryStream();
+                    filestream.CopyTo(memorystream);
+                    model.Picture = memorystream.ToArray();
+                }
+                _context.Copouns.Update(model);
+                await _context.SaveChangesAsync();
+                _toastNotification.AddAlertToastMessage("Copoun updated Succesfully");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
         }
     }
 }
